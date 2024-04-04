63de77a83dec25c94787397cf1b5985708f31aca
German Maglione <gmaglione@redhat.com>
2024-04-04 15:52:25 +0200

    Fix brew test checks
    Signed-off-by: German Maglione <gmaglione@redhat.com>

--- Formula/podman-bootc.rb
+++ Formula/podman-bootc.rb
@@ -1,14 +1,16 @@
 class PodmanBootc < Formula
   desc "Streamlining podman + bootc interactions"
   homepage "https://gitlab.com/bootc-org/podman-bootc-cli"
-  url "https://gitlab.com/bootc-org/podman-bootc-cli.git", tag: "v0.1.1"
+  url "https://gitlab.com/bootc-org/podman-bootc-cli.git",
+    tag: "v0.1.1",
+    revision: "5754e1774e357d4ff872b66e3462afed265f41a8"
   license "Apache-2.0"
 
   depends_on "go" => :build
   depends_on "libvirt"
-  depends_on "xorriso"
   # Works on linux but for now we just provide a formula for MacOS
   depends_on arch: :arm64
+  depends_on "xorriso"
 
   def install
     system "make"
