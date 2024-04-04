eef013771949d5e5b2569f0a3f61a8805bbbfebc
German Maglione <gmaglione@redhat.com>
2024-04-04 11:54:25 +0200

    Fix missing branch and dependency
    Homebrew expect the main branch to be called "master",
    but on github it's called "main".
    
    Add glibc as dependency, it seems it no defined as
    libvirt's dependecy.
    
    Signed-off-by: German Maglione <gmaglione@redhat.com>

--- Formula/podman-bootc.rb
+++ Formula/podman-bootc.rb
@@ -1,11 +1,11 @@
 class PodmanBootc < Formula
   desc "Streamlining podman + bootc interactions"
   homepage "https://gitlab.com/bootc-org/podman-bootc-cli"
-  url "https://gitlab.com/bootc-org/podman-bootc-cli.git"
-  version "0.1.1"
+  url "https://gitlab.com/bootc-org/podman-bootc-cli.git", tag: "v0.1.1"
   license "Apache-2.0"
 
   depends_on "go" => :build
+  depends_on "glibc"
   depends_on "libvirt"
   depends_on "xorriso"
 
